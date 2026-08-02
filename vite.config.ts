3f8b128d705bbb3dcc12094e613183ea625f48d9
Julian Leitner <yJulian@outlook.de>
2026-08-02 21:38:25 +0200

    Update Icon

--- vite.config.ts
+++ vite.config.ts
@@ -47,15 +47,9 @@ export default defineConfig({
         scope: '.',
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
+            src: 'vite.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
           }
         ],
       },
